6e143705e9e699ecdbcdecc04423f8d571290d6d
Keith Millar <keithmillar@hotmail.ca>
2020-06-17 23:58:28 +0000

    cleaned up client.js a bit

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -31,70 +31,39 @@ const data = [
 ];
 
 const renderTweets = function(tweets) {
-  // loops through tweets
-
   for (let tweet of tweets) {
     $('.tweets-list').append(createTweetElement(tweet));
   }
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
 };
 
 const createTweetElement = function(tweet) {
   // let d = new Date()
   // let datePosted = tweet.created_at - d.getTime();
 
-  // console.log(datePosted)
-
-  
   let $tweet = `
   <article class="tweet-container">
-  <header class="tweet-header">
-    <div>
-      <img src="${tweet.user.avatars}" alt="https://i.imgur.com/5fUVPRP.png">
-      <h3 class="tweet-username">${tweet.user.name}</h3>
-    </div>
-    <span class="handle">${tweet.user.handle}</span>
-  </header>
-    <p>${tweet.content.text}</p>
-    <hr>
-  <footer class="tweet-footer">
-    <p>${tweet.created_at}</p>
-    <span class="action-icons">
-      <i class="fa fa-flag" aria-hidden="true"></i>
-      <i class="fa fa-retweet" aria-hidden="true"></i>
-      <i class="fa fa-heart" aria-hidden="true"></i>
-    </span>
-  </footer>
-</article>`;
-
-  // let result = $tweet.addClass('test-class');
+    <header class="tweet-header">
+      <div>
+        <img src="${tweet.user.avatars}" alt="https://i.imgur.com/5fUVPRP.png">
+        <h3 class="tweet-username">${tweet.user.name}</h3>
+      </div>
+      <span class="handle">${tweet.user.handle}</span>
+    </header>
+      <p>${tweet.content.text}</p>
+      <hr>
+    <footer class="tweet-footer">
+      <p>${tweet.created_at}</p>
+      <span class="action-icons">
+        <i class="fa fa-flag" aria-hidden="true"></i>
+        <i class="fa fa-retweet" aria-hidden="true"></i>
+        <i class="fa fa-heart" aria-hidden="true"></i>
+      </span>
+    </footer>
+  </article>`;
 
   return $tweet;
 };
 
-
 $(document).ready(function() {
   renderTweets(data);
 });
-
-
-// <article class="tweet-container">
-//   <header class="tweet-header">
-//     <div>
-//       <img src="https://i.imgur.com/73hZDYK.png" alt="https://i.imgur.com/5fUVPRP.png">
-//       <h3 class="tweet-username">Newton</h3>
-//     </div>
-//     <span class="handle">@SirIsaac</span>
-//   </header>
-//     <p>If I have seen further it is by standing on the shoulders of giants</p>
-//     <hr>
-//   <footer class="tweet-footer">
-//     <p>10 days ago</p>
-//     <span class="action-icons">
-//       <i class="fa fa-flag" aria-hidden="true"></i>
-//       <i class="fa fa-retweet" aria-hidden="true"></i>
-//       <i class="fa fa-heart" aria-hidden="true"></i>
-//     </span>
-//   </footer>
-// </article>
